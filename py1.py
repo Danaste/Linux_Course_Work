071f5ef20a15a08c51cd749074b263788183898d
Dana Steward <danasteward4848@gmail.com>
2025-03-03 12:18:08 +0200

    Adding py1.py, q2_output.txt, requirements.txt, and linux_history.txt

---
+++ py1.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+numbers = np.array([5, 10, 15, 20])
+
+mean = np.mean(numbers)
+std_dev = np.std(numbers)
+
+
+print("Mean of the array:", mean)
+print("Standard Deviation of the array:", std_dev)
